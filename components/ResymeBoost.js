addf9dc90ccc27939a9a4513a6cf9af053788824
Mike Koller <mkoller@eml.cc>
2022-12-17 17:18:28 -0700

    Style the job description form

--- components/ResymeBoost.js
+++ components/ResymeBoost.js
@@ -1,11 +1,20 @@
 const ResymeBoost = () => {
   return (
-    <div className="flex flex-col items-center p-2 py-16">
+    <div className="max-width-[768px] flex flex-col items-center p-2 py-16">
       <h1 className="text-4xl tracking-wider font-bold pb-4">Resyme Boost</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-        doloremque!
-      </p>
+      <form className="py-8 text-right">
+        <p className="py-2 text-left">Enter job description:</p>
+        <div>
+          <textarea
+            name="job"
+            rows="10"
+            className="w-[300px] sm:w-[600px] border-none rounded-[3px] text-black pl-1 mb-4"
+          ></textarea>
+        </div>
+        <button className="bg-[#e0ffff] text-[#5f7470] py-1 px-2 rounded-sm">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
