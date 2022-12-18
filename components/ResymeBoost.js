6c7e56e1fa7296d3116ca9fae07f7b0d1eba10e4
Mike Koller <mkoller@eml.cc>
2022-12-18 10:43:26 -0700

    Create resume textarea & functionality to handle both inputs

--- components/ResymeBoost.js
+++ components/ResymeBoost.js
@@ -2,18 +2,16 @@ import { useState } from "react";
 
 const ResymeBoost = () => {
   const [userInput, setUserInput] = useState("");
+  const [resume, setResume] = useState("");
   const [apiOutput, setApiOutput] = useState("");
-  const [isGenerating, setIsGenerating] = useState(false);
   const callGenerateEndpoint = async () => {
-    setIsGenerating(true);
-
     console.log("Calling OpenAI...");
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ userInput }),
+      body: JSON.stringify({ userInput, resume }),
     });
 
     const data = await response.json();
@@ -21,20 +19,36 @@ const ResymeBoost = () => {
     console.log("OpenAI replied...", output.text);
 
     setApiOutput(`${output.text}`);
-    setIsGenerating(false);
+    setUserInput("");
+    setResume("");
   };
   const onUserChangedText = (e) => {
     setUserInput(e.target.value);
   };
+  const onResumeChanged = (e) => {
+    setResume(e.target.value);
+  };
   return (
     <div className="flex flex-col items-center p-2 py-16">
       <h1 className="text-4xl tracking-wider font-bold pb-4">Resyme Boost</h1>
+      <h2 className="py-2 font-bold text-[#FFFACD]">
+        Resume Summary Creator: Tailor Your Resume for Any Job
+      </h2>
       <form className="py-8 text-right">
-        <p className="py-2 text-left">Enter job description:</p>
+        <p className="py-2 text-left">Paste job description:</p>
         <div>
           <textarea
             value={userInput}
             onChange={onUserChangedText}
+            rows="10"
+            className="w-[300px] sm:w-[600px] border-none rounded-[3px] text-black pl-1 mb-4"
+          ></textarea>
+        </div>
+        <p className="py-2 text-left">Paste resume:</p>
+        <div>
+          <textarea
+            value={resume}
+            onChange={onResumeChanged}
             rows="10"
             className="w-[300px] sm:w-[600px] border-none rounded-[3px] text-black pl-1 mb-4"
           ></textarea>
